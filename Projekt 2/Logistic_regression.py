71e3da618b81aec2d9303f0703e56234fa389a97
buurdrengen <s203947@student.dtu.dk>
2022-10-31 17:47:33 +0100

    log_nolambda

--- Projekt 2/Logistic_regression.py
+++ Projekt 2/Logistic_regression.py
@@ -14,19 +14,19 @@ model = model.fit(X,y)
 
 # Classify CHD-response as "No CHD"/"CHD" (0/1) and assess probabilities
 y_est = model.predict(X)
-y_est_white_prob = model.predict_proba(X)[:,0]
+y_est_healthy_prob = model.predict_proba(X)[:,0]
 print('y_estimated:',y_est)
-print('y_white_estimated',y_est_white_prob)
+print('y_healthy_estimated:',y_est_healthy_prob)
 
-print('misses',np.sum(y_est != y))
-print(float(len(y_est)))
+misses = np.sum(y_est != y)
+print(f'Amount of misses: {misses} of {len(y_est)}.')
 # Define a new data object (using first patient from the data.csv)
 x = np.array([160, 12, 5.73, 23.11, 1, 49, 25.30, 97.20, 52]).reshape(1,-1)
 # Evaluate the probability of x having CHD: 
 x_class = model.predict_proba(x)[0,1]
 
 # Evaluate classifier's misclassification rate over entire training data
-misclass_rate = np.sum(y_est != y) / len(y_est)
+misclass_rate = np.sum(y_est != y) / float(len(y_est))
 
 # Display classification results
 print('\nProbability of given sample having CHD: {0:.4f}'.format(x_class))
@@ -34,9 +34,9 @@ print('\nOverall misclassification rate: {0:.3f}'.format(misclass_rate))
 
 f = figure();
 class0_ids = np.nonzero(y==0)[0].tolist()
-plot(class0_ids, y_est_white_prob[class0_ids], '.y')
+plot(class0_ids, y_est_healthy_prob[class0_ids], '.y')
 class1_ids = np.nonzero(y==1)[0].tolist()
-plot(class1_ids, y_est_white_prob[class1_ids], '.r')
+plot(class1_ids, y_est_healthy_prob[class1_ids], '.r')
 xlabel('Data object (patient)'); ylabel('Predicted prob. of class "No-CHD"');
 legend(['No CHD', 'CHD'])
 ylim(-0.01,1.5)
