64d27a66f552db62bba82d9f369c47e3d08368ef
karenwjensen <113354172+karenwjensen@users.noreply.github.com>
2022-11-03 11:40:22 +0100

    part a

--- regression1.py
+++ regression1.py
@@ -8,7 +8,7 @@ Created on Mon Oct 31 13:16:52 2022
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 27 09:55:05 2022
-@author: ESL
+@author: karen
 """
 
 #tranforming data so mean = 0, STD = 1
@@ -16,7 +16,7 @@ import numpy as np
 import pandas as pd
 from sklearn import linear_model
 from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend, 
-                           title, subplot, show, grid,savefig)
+                           title, subplot, show, grid,savefig,plot)
 from scipy.io import loadmat
 import sklearn.linear_model as lm
 from sklearn import model_selection
@@ -24,12 +24,13 @@ from toolbox_02450 import rlr_validate
 
 filename = 'data.csv'
 df = pd.read_csv(filename)
-df2 = df.apply(pd.to_numeric,errors='coerce')
+#df2 = df.apply(pd.to_numeric,errors='coerce')
 
-raw_data = df2.values  
+raw_data = df.values  
 
-cols = range(1, 11) 
+cols = range(1, 10) 
 X = raw_data[:,cols]
+y = raw_data[:,10]
 
 #Transform mean=0 og STD=1
 #Transform mean=0 og STD=1 for each column except binary data
@@ -39,41 +40,33 @@ for i in range(4):
     X[:,i+5] = (X[:,i+5]-np.mean(X[:,i+5]))/np.std(X[:,i+5])
 
 
-
-X = (X-np.mean(X))/np.std(X)
-
-
-#%%
-attributeNames = np.asarray(df.columns[cols])
-
-classLabels = X[:,-1] # -1 takes the last column
-
-classNames = np.unique(classLabels)
-
-# Python dictionary as so:
-classDict = dict(zip(classNames,range(len(classNames))))
-
-
-#This is the class index vector y:
-y = np.array([classDict[cl] for cl in classLabels])
-
-
-#%%
-## Crossvalidation
+## K = 10 fold Crossvalidation
 
 # Values of lambda
-lambdas = np.power(100.,range(-3,3))
-#%%
+lambdas = np.power(10.,range(-1,5))
+
 A = rlr_validate(X,y,lambdas,cvf=10)
 
-
 # opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
-figure()
+
+#Optimal generalization error
+print('Optimal value of error:' , A[0])
+print('Optimal value of lambda:' ,A[1])
+
+figure(1)
 title('Optimal lambda: 1e{0}'.format(np.log10(A[1])))
 loglog(lambdas,A[3].T,'b.-',lambdas,A[4].T,'r.-')
 xlabel('Regularization factor')
-ylabel('Squared error (crossvalidation)')
-legend(['Train error','Validation error'])
+ylabel('Squared error (10 fold crossvalidation)')
+legend(['Train error','Test error'])
 grid()
 savefig('regression1.png')
 show()
+
+figure(2)
+title('Effects of the selected attributes')
+semilogx(lambdas,A[2].T[:,1:],'.-') # Don't plot the bias term
+xlabel('Regularization factor')
+ylabel('Mean Coefficient Values')
+grid()
+savefig('weigth1.png')
